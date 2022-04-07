e197236df833170ce66e31d5ceff571c44737ec6
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-04-07 04:58:32 -0300

    estilizando usando children

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,10 +4,14 @@ import Title from '../src/components/title/Title'
 function HomePage (){
     return (
         <div>
-            <Title />
-            <Title></Title>
-            <Title />
-            Hello world testiiii22222
+            {/* <Title text="Um Texto" />
+            <p>paragrafo 1</p>
+            <Title text="Outro Texto"></Title>
+            <p>pagrafo 2</p>
+            <Title text="Outro outro texto"/>
+            <p>pagrafo 3</p> */}
+            <Title>Filho do titleee</Title>
+            <p>pagrafo 4</p>
         </div>
     )
 }
