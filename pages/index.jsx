011ba94a8beb66d5f58c799ea6d71ccaead10319
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-04-07 18:02:18 -0300

    aula estados

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,27 @@
+import { useState } from 'react'
+import Head from 'next/head'
+
 import Title from '../src/components/title/Title'
 import Subtitle from '../src/components/subtitle/Subtitle'
-import Head from 'next/head'
+import Button from '../src/components/button/Button'
+
+
 
 
 function HomePage (){
+
+    //nome da variavel //função que altera a variavel
+    const [click, setClick] = useState(0)
+   
+   
+    const handleClick = () => {
+
+        setClick(click + 1)
+    }
+
+
+
+
     return (
         <>
             <Head>
@@ -16,7 +34,11 @@ function HomePage (){
 
                 <Title text="Um Texto"></Title>
                 <Subtitle/>
-
+                <Button
+                    onClick = {handleClick}                    
+                > 
+                Botão Teste </Button>
+                <p>Numero de clicks: {click} </p>
             </div>
             
         </>
