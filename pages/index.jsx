e714806ca0f6ecde2c689c7dde702abe8999270a
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-04-07 04:21:16 -0300

    aja

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,15 @@
+import Title from '../src/components/title/Title'
+
+
 function HomePage (){
     return (
         <div>
+            <Title />
+            <Title></Title>
+            <Title />
             Hello world testiiii22222
         </div>
     )
 }
 
 export default HomePage
-
---
+++ src/components/title/Title.jsx
@@ -0,0 +1,8 @@
+function Title () {
+    return(
+    <h1>Meu titulo h1</h1>
+    )
+}
+
+export default Title
+
